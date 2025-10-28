79572f3bbd99140c0489f48fcf3c9555232d1831
kmj0973 <kmj0973@naver.com>
2025-10-28 15:32:48 +0900

    chore : figma 디자인 시스템 colors 세팅

--- devTime/src/App.tsx
+++ devTime/src/App.tsx
@@ -2,8 +2,9 @@ import { Button } from './components/ui/button'
 
 function App() {
   return (
-    <div>app
+    <div className='bg-primary-10'>app
       <Button>hi</Button>
+      <div className='bg-gray-100'>10%</div>
     </div>
   )
 }
