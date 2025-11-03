6c87d5d3a5c9254d4906073dc808e806bd8558c0
kmj0973 <a22950973@gmail.com>
2025-11-03 16:44:50 +0900

    feat : 모달 zustand 세팅

--- devTime/src/features/auth/ui/login-required-dialog/LoginRequiredDialog.tsx
+++ devTime/src/features/auth/ui/login-required-dialog/LoginRequiredDialog.tsx
@@ -1,6 +1,9 @@
+import useModalStore from '@/shared/store/useModalStroe';
 import { Link } from 'react-router-dom';
 
 export default function LoginRequiredDialog() {
+  const closeModal = useModalStore((state) => state.closeModal);
+
   return (
     <div className='fixed inset-0 flex items-center justify-center bg-state-dim1'>
       <div className='bg-white p-6 rounded-[12px] shadow-lg max-w-[328px] w-full'>
@@ -9,10 +12,14 @@ export default function LoginRequiredDialog() {
           DevTime을 사용하려면 로그인이 필요합니다. 로그인 페이지로 이동할까요?
         </p>
         <div className='flex justify-end gap-4'>
-          <div className='px-4 py-[13px] bg-gray-100 text-subtitle-s text-primary rounded-[5px] hover:bg-gray-400 transition'>
+          <div
+            onClick={() => closeModal()}
+            className='px-4 py-[13px] bg-gray-100 text-subtitle-s text-primary rounded-[5px] hover:bg-gray-400 transition'
+          >
             취소
           </div>
           <Link
+            onClick={() => closeModal()}
             to='/login'
             className='px-4 py-[13px] bg-primary text-subtitle-s text-white rounded-[5px] hover:bg-blue-700 transition'
           >
