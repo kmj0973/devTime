4e5a62594c3380e98790e7f1e4d1d9ea6039d930
kmj0973 <kmj0973@naver.com>
2025-11-10 17:58:42 +0900

    feat : 회원가입 기능 구현

--- devTime/src/features/signup/hooks/useSignUpForm.ts
+++ devTime/src/features/signup/hooks/useSignUpForm.ts
@@ -1,13 +1,16 @@
 import { useForm, type SubmitHandler } from 'react-hook-form';
 import { signupFormSchema, type SignUpFormFields } from '../model/schema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { requestSignUp } from '../api/requests';
+import { requestEmailCheck, requestNicknameCheck, requestSignUp } from '../api/requests';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const useSignUpForm = () => {
   const {
     register,
     handleSubmit,
     watch,
+    trigger,
     setError,
     clearErrors,
     formState: { isValid, errors },
@@ -20,19 +23,84 @@ export const useSignUpForm = () => {
       accept: false,
     },
     resolver: zodResolver(signupFormSchema),
+    shouldFocusError: false,
     mode: 'onChange',
   });
   const [email, nickname, accept] = watch(['email', 'nickname', 'accept']);
+  const [isEmailChecked, setIsEmailChecked] = useState(false);
+  const [isNicknameChecked, setIsNicknameChecked] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsEmailChecked(false);
+  }, [email]);
+
+  useEffect(() => {
+    setIsNicknameChecked(false);
+  }, [nickname]);
 
   const handleEmailCheck = async () => {
-    console.log('email check');
+    const isValidEmail = await trigger('email');
+    if (!isValidEmail) return;
+
+    const response = await requestEmailCheck(email);
+
+    if (response.available) {
+      setIsEmailChecked(true);
+      clearErrors('email');
+    } else {
+      setError('email', { message: '이미 사용 중인 이메일입니다.' });
+    }
   };
+
   const handleEmailNickname = async () => {
-    console.log('nickname check');
+    const isValidNickname = await trigger('nickname');
+    if (!isValidNickname) return;
+
+    const response = await requestNicknameCheck(nickname);
+
+    if (response.available) {
+      setIsNicknameChecked(true);
+      clearErrors('nickname');
+    } else {
+      setError('nickname', { message: '이미 사용 중인 닉네임입니다.' });
+    }
+  };
+
+  const onCheckEmail = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (errors.email) {
+      return;
+    }
+    if (!isEmailChecked && email) {
+      setError('email', { type: 'duplicate', message: '중복 확인을 해주세요.' });
+      return;
+    }
+  };
+
+  const onCheckNickname = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (errors.nickname) {
+      return;
+    }
+    if (!isNicknameChecked && nickname) {
+      setError('nickname', { type: 'duplicate', message: '중복 확인을 해주세요.' });
+      return;
+    }
   };
 
   const onSubmit: SubmitHandler<SignUpFormFields> = async (data) => {
-    await requestSignUp(data);
+    try {
+      const response = await requestSignUp(data);
+      if (response.success) {
+        console.log('회원가입성공');
+        navigate('/login');
+      } else {
+        console.log('회원가입 실패:', response.message);
+      }
+    } catch (error) {
+      console.error('회원가입 중 오류 발생:', error);
+    }
   };
 
   return {
@@ -43,8 +111,12 @@ export const useSignUpForm = () => {
     email,
     nickname,
     accept,
+    isEmailChecked,
+    isNicknameChecked,
     handleEmailCheck,
     handleEmailNickname,
+    onCheckEmail,
+    onCheckNickname,
     onSubmit,
   };
 };
