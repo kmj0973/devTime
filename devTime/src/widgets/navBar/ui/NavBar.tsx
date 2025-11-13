7050303ee303f83d8299fe970ac6f8d7a5d9652a
kmj0973 <kmj0973@naver.com>
2025-11-13 15:36:00 +0900

    feat : 로그인 여부 navBar 변경 및 로그아웃 기능 추가

--- devTime/src/widgets/navBar/ui/NavBar.tsx
+++ devTime/src/widgets/navBar/ui/NavBar.tsx
@@ -1,7 +1,24 @@
 import navBarLogo from '@/assets/navBarLogo.svg';
+import useAuthStore from '@/shared/store/useAuthStore';
 import { Link, NavLink } from 'react-router-dom';
+import UserProfileImage from './UserProfileImage';
+import { requestLogout } from '@/features/auth/api/requests';
 
 export default function NavBar() {
+  const accessToken = useAuthStore((state) => state.accessToken);
+  const userInfo = useAuthStore((state) => state.user);
+  const logout = useAuthStore((state) => state.logout);
+
+  const onLogout = async () => {
+    if (!accessToken) {
+      console.log('로그아웃 실패');
+      return;
+    }
+    await requestLogout(accessToken);
+    logout();
+    window.location.reload();
+  };
+
   return (
     <div className='max-w-[1200px] h-10 mx-auto flex justify-between  items-center'>
       <div className='flex gap-12'>
@@ -21,12 +38,22 @@ export default function NavBar() {
         </div>
       </div>
       <div className='flex justify-center items-center gap-9'>
-        <Link className='text-body-s text-secondary-indigo' to='/login'>
-          로그인
-        </Link>
-        <Link className='text-body-s text-secondary-indigo' to='/signup'>
-          회원가입
-        </Link>
+        {userInfo ? (
+          <div className='flex justify-center items-center gap-3'>
+            <UserProfileImage />
+            <div className='text-secondary-indigo text-body-b'>{userInfo.nickname}</div>
+            <button onClick={onLogout}>logout</button>
+          </div>
+        ) : (
+          <>
+            <Link className='text-body-s text-secondary-indigo' to='/login'>
+              로그인
+            </Link>
+            <Link className='text-body-s text-secondary-indigo' to='/signup'>
+              회원가입
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
