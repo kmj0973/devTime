eb1d54fd14eb84d30c3e293399e50d1e05d47319
kmj0973 <a22950973@gmail.com>
2025-12-01 17:28:20 +0900

    feat : 투두리스트 다이얼로그 UI 구현

--- devTime/src/pages/home/ui/HomePage.tsx
+++ devTime/src/pages/home/ui/HomePage.tsx
@@ -1,3 +1,4 @@
+import TodoListDialog from '@/features/timer/ui/TodoListDialog';
 import NavBar from '@/widgets/navBar/ui/NavBar';
 import Timer from '@/widgets/timer/ui/Timer';
 import TimerButton from '@/widgets/timer/ui/TimerButton';
@@ -7,6 +8,7 @@ export default function HomePage() {
   return (
     <div className='h-screen items-center bg-linear-to-b from-[#F6F7F9] to-[#E9ECF5] pt-4'>
       <NavBar />
+      <TodoListDialog />
       <div className='flex flex-col justify-center items-center'>
         <TimerText />
         <Timer />
