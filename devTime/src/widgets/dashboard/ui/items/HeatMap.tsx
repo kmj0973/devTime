1abf959ad9628b495b0d82fb48ebee664eee6db8
kmj0973 <kmj0973@naver.com>
2025-12-19 17:41:06 +0900

    feat : 대시보드 히트맵 기능 및 컴포넌트 완성

--- devTime/src/widgets/dashboard/ui/items/HeatMap.tsx
+++ devTime/src/widgets/dashboard/ui/items/HeatMap.tsx
@@ -10,23 +10,32 @@ type MonthLabel = {
   end: number;
 };
 
+type HeatMapData = {
+  date: string;
+  studyTimeHours: number;
+  colorLevel: number;
+};
+
 export default function HeatMap() {
-  const [d, setD] = useState([]);
+  const [mergedData, setMergedData] = useState<Record<string, HeatMapData>>({});
 
   const today = new Date();
   const oneYearAgo = subYears(today, 1);
 
+  //오늘 기준으로 1년 전의 날들을 배열로 전달
   const days = eachDayOfInterval({
     start: oneYearAgo,
     end: today,
   });
 
+  // 전달된 날짜 배열을 주단위로 변환
   const results = toHeatMapMatrix(days);
 
+  // 월 별 라벨
   const monthLabels: MonthLabel[] = [];
 
   let prevLabel: string | null = null;
-  let startCol = 2; // 요일 라벨은 1번 column
+  let startCol = 2;
 
   results.forEach((week, weekIndex) => {
     const firstDay = week[0];
@@ -65,14 +74,34 @@ export default function HeatMap() {
   useEffect(() => {
     const getHeatMap = async () => {
       const result = await requestGetHeatMap();
-      setD(result.heatmap);
+
+      const mergedData = result.heatmap.reduce(
+        (acc: Record<string, HeatMapData>, curr: HeatMapData) => {
+          const date = curr.date;
+          if (!acc[date]) {
+            acc[date] = {
+              date: curr.date,
+              studyTimeHours: curr.studyTimeHours,
+              colorLevel: curr.colorLevel,
+            };
+          } else {
+            acc[date] = {
+              date: curr.date,
+              studyTimeHours: acc[date].studyTimeHours + curr.studyTimeHours,
+              colorLevel: curr.colorLevel,
+            };
+          }
+          return acc;
+        },
+        {},
+      );
+
+      setMergedData(mergedData);
     };
 
     getHeatMap(); //같은 날짜는 합치고(합칠 때 시간이 나오기때문에 가장 높은 시간을 가진 객체만 남기기),
     //레벨도 내가 다시 객체로 만들기 { 2024-06-01: { time:000, level: 2 } }
   }, []);
-
-  console.log(d);
 
   return (
     <div className='bg-white w-full min-w-[800px] h-full min-h-[306px] col-span-3 rounded-[18px] p-6 pr-15'>
@@ -107,8 +136,26 @@ export default function HeatMap() {
 
         {/* --- 달력 칸 출력 --- */}
         {Array.from({ length: 7 }).map((_, dayIndex) =>
-          results.map((week, weekIndex) =>
-            week[dayIndex] === null ? (
+          results.map((week, weekIndex) => {
+            let level = null;
+            let hh = '00';
+            let mm = '00';
+            let ss = '00';
+
+            const formatDate = format(week[dayIndex], 'yyyy-MM-dd');
+
+            if (formatDate in mergedData) {
+              level = mergedData[formatDate].colorLevel;
+              console.log(mergedData[formatDate]);
+
+              const totalSeconds = Math.floor(mergedData[formatDate].studyTimeHours * 3600);
+
+              hh = String(Math.floor(totalSeconds / 3600)).padStart(2, '0');
+              mm = String(Math.floor((totalSeconds % 3600) / 60)).padStart(2, '0');
+              ss = String(totalSeconds % 60).padStart(2, '0');
+            }
+
+            return week[dayIndex] === null ? (
               <div
                 key={`empty-${dayIndex}-${weekIndex}`}
                 style={{
@@ -127,19 +174,19 @@ export default function HeatMap() {
                   gridColumn: weekIndex + 2,
                   gridRow: dayIndex + 2,
                 }}
-                className='w-[18px] h-[18px] bg-gray-50 border border-gray-300 rounded-[5px]'
+                className={`w-[18px] h-[18px] ${level != null ? `bg-heatmap-level-${level + 1}` : 'bg-gray-50'} border border-gray-300 rounded-[5px]`}
               >
                 <div
                   className='absolute w-[124px] h-5 z-1 opacity-0 hover:opacity-100 whitespace-nowrap
             transition-all duration-100'
                 >
                   <div className='absolute top-6 left-5 bg-black w-[124px] text-body-s-r text-white text-center rounded-[3px] px-2 py-1'>
-                    {week[dayIndex]?.toDateString()}
+                    {hh}시간 {mm}분 {ss}초
                   </div>
                 </div>
               </div>
-            ),
-          ),
+            );
+          }),
         )}
       </div>
 
