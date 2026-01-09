a9353eb8d23d5f5c52e16694da11e7032abf5fb6
kmj0973 <kmj0973@naver.com>
2026-01-09 17:35:28 +0900

    feat : error boundary 추가

--- devTime/src/app/App.tsx
+++ devTime/src/app/App.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import Router from './providers/RouterProvider';
 import { NavBar } from '@/widgets/navBar/ui/NavBar';
 
 function App() {
   return (
-    <>
+    <ErrorBoundary fallback={<div>에러발생</div>}>
       <Suspense
         fallback={
           <div className='h-screen items-center bg-linear-to-b from-[#F6F7F9] to-[#E9ECF5] pt-4'>
@@ -14,7 +15,7 @@ function App() {
       >
         <Router />
       </Suspense>
-    </>
+    </ErrorBoundary>
   );
 }
 
